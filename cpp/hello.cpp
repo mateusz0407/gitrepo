1395c17ec30b839ebc36ed6190ec4e9fa9da08df
mateusz0407 <jackowskim2000@wp.pl>
2017-09-25 12:37:44 +0200

    kody 2a

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -10,7 +10,20 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	cout << "Witaj w C++!!";
-	return 0;
+ //char imie; // deklaracja zminnej znakowej 
+ char imie[10]; // deklaracja zminnej tablicowej  
+ int wiek;
+ wiek = 0; //inicjacja zmiennej
+
+	cout << "Witaj w C++!!" << endl;
+	cout << "Podaj imie: ";
+	//cin >> imie;
+    cin.getline (imie, 10);
+    cout << "cześć, " << imie << "!" << endl;
+    cin.ignore();
+    cout << "ile masz lat??? ";
+    cin >> wiek;
+    cout << "urodziłeś się w " << 2017 - wiek << endl;
+    return 0;
 }
 
