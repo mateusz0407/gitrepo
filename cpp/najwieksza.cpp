9dedc1baa3f1fed793027bd0517d7a98268e209c
mateusz0407 <jackowskim2000@wp.pl>
2017-09-26 10:38:03 +0200

    kody c++ 2

---
+++ cpp/najwieksza.cpp
@@ -0,0 +1,32 @@
+/*
+ * hello.cpp
+
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+ //char imie; // deklaracja zminnej znakowej 
+; // deklaracja zminnej tablicowej  
+ int a , b , c ;
+ a =b=c=0;
+
+    cout << "podaj liczby: " << endl;
+    cin >> a >> b >> c;
+    if (a > b)
+{
+    if (a>c) cout <<"najwieksze jest a!";
+    else cout << "najwieksze jest c";
+    } 
+    else
+    {
+    ; // a nie jest większe od b
+}
+    
+    return 0;
+}
+
