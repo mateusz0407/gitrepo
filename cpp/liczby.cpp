99fd8253d0b3c06a4685e48deac61ebf0ce7de5b
mateusz0407 <jackowskim2000@wp.pl>
2017-10-02 22:09:59 +0200

    praca domowa

---
+++ cpp/liczby.cpp
@@ -0,0 +1,29 @@
+
+
+#include <iostream>
+using namespace std;
+main()
+{
+	float suma=0;
+	int ilosc=0;
+	int a=0;
+	cout<<"wpisuj liczby tylko całkowite"<<endl;
+	cout<<"podaj ilość liczb: "<<endl;
+	cin>>ilosc;
+	cout<<"podaj liczby: "<<endl;
+	for (int i=0;i<ilosc;i++)
+	{
+	cin>>a;
+	if(a<=0)
+	{
+	i=i-1;
+	cout<<"podaj liczbe wiekszą od zera!"<<endl;
+	}
+	else
+	suma=suma+a;
+	}
+	cout<<"suma tych cyfr wynosi: "<<suma<<"a ich srednia to:"<<suma/ilosc<<endl;
+	return 0;
+}
+
+
