0e44bfb691e3df2c0dd9a954a81515ae714aede1
mateusz0407 <jackowskim2000@wp.pl>
2017-10-24 10:38:55 +0200

    dobre

--- cpp/tabele.cpp
+++ cpp/tabele.cpp
@@ -21,13 +21,25 @@ void sumuj (int tab[], int ile){
     }
     cout<<"suma liczb: "<<suma<<endl;
 }
+
 void najmniejsza(int tab[], int ile){
-	int mini=tab[0];
-	for(int i = 0; i < mini;i++){
-	if(mini >tab[i])
-		mini=tab[i];
-	}
-	cout<<"najmniejsza iczba : "<<mini<<endl;
+    int mini=tab[0];
+    int i =0;
+    for(i =1; i < mini;i++){
+    if(mini >tab[i])
+        mini=tab[i];
+    }
+    cout<<"najmniejsza iczba : "<<mini<<endl;
+    }
+    
+void podzielna(int tab[], int ile){
+    int i;
+    int licznik=0;
+    for (i=0;i<ile;i++){
+    if (tab[i] % 5 == 0)
+        licznik++;
+    }
+    cout<<"Podzielnych przez 5: "<< licznik <<endl;
     }
 
 
@@ -42,6 +54,7 @@ int main(int argc, char **argv)
     pobierzliczby(liczby,rozmiar);
     sumuj(liczby,rozmiar);
     najmniejsza(liczby, rozmiar);
+    podzielna(liczby, rozmiar);
     return 0;
 }
 
