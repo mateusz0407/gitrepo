9e3d951cf63414c3a61a39f4a91acdd2c57849a9
mateusz0407 <jackowskim2000@wp.pl>
2017-11-07 10:37:15 +0100

    praca na lekcji

--- dlotek.py
+++ dlotek.py
@@ -2,31 +2,37 @@
 # -*- coding: utf-8 -*-
 import random
 
-def losuj(ileliczb,maksliczby):
-    liczby = [] #pusta lista
 
-    ile = 0 #liczba unikalnych liczb
-    #for i in range (ileliczb):
+def losuj(ileliczb, maksliczb):
+    liczby = []  # pusta lista
+
+    ile = 0  # liczba unikalnych liczb
+    # for i in range (ileliczb):
+
     while ile < ileliczb:
-        liczba = random.randint(0 , maksliczby)
-        if liczby.count(liczba)==0:
+        liczba = random.randint(0, maksliczb)
+        if liczby.count(liczba) == 0:
                 liczby.append(liczba)
-                ile +=1
+                ile += 1
     print(liczby)
     return liczby
 
+
 def main(args):
-    ileliczb = int(input ("ile liczb chce zgadywac?"))
-    maksliczby = int(input ("maksymalna losowana liczba: "))
+    ileliczb = int(input("ile liczb chce zgadywac?"))
+    maksliczb = int(input("maksymalna losowana liczba: "))
 
-    liczby = losuj(ileliczb , maksliczby)
+    liczby = losuj(ileliczb, maksliczb)
 
-    #pobieraie typów użytkownika
-    typy = set() # pusty zbiór
-    for i in random(ileliczb):
+    # pobieraie typów użytkownika
+    typy = set()  # pusty zbiór
+    # for i in random(ileliczb):
+    ile = 0
+    while ile < ileliczb:
         typ = input("podaj typ: ")
-        typy.add(typ)
-
+        if typ not in typy:
+            typy.add(typ)
+            ile += 1
     print(typy)
     return 0
 
