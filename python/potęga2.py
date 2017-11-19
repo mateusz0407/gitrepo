abaa059ff6eac101fd83011a83aaf7870801a0b6
mateusz0407 <jackowskim2000@wp.pl>
2017-11-19 17:41:38 +0100

    praca domowa

--- python/potęga2.py
+++ python/potęga2.py
@@ -5,21 +5,23 @@
 #  
 
 def potega_it(a,b):
-	
 	i = 1 
 	wynik = 1
 	while i <= b :
 		wynik = wynik * a
 		i = i+1
-	
 	return wynik
     
-
 def main(args):
 	a = float(input("Podaj podstawę"))
 	b = int(input("Podaj wykładnik"))
+	assert potega_it(4,1) == 4
+	assert potega_it(2,2) == 4
+	assert potega_it(3,3) == 27
+	
 	print ("wynik: ", potega_it(a,b))
 	
+	return 0
 	
 if __name__ == '__main__':
     import sys
