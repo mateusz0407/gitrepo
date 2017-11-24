10ffaec916ce8715c21fda946d00bbb1d2294547
mateusz0407 <jackowskim2000@wp.pl>
2017-11-24 08:46:06 +0100

    minmax

--- python/minmax.py
+++ python/minmax.py
@@ -23,13 +23,32 @@ def minimum(lista):
     """wyszukwanie minimum"""
     min = lista[0]
     for i, el in enumerate(lista):
+        if el < min:
+            min = el
         print(i, el)
-    return 0
+
+    return min
+
+
+def maksimum(lista):
+    # wyszukwanie minimum
+    max = lista[0]
+    for i, el in enumerate(lista):
+        if el > max:
+            max = el
+        print(i, el)
+
+    return max
 
 
 def main(args):
     lista = losuj(20, 50)
+    assert minimum([7, 5, 3, 1]) == 1
+    assert maksimum([7, 5, 3, 1]) == 7
+
     print("Min: ", minimum(lista))
+    print("Max: ", maksimum(lista))
+
     return 0
 
 
