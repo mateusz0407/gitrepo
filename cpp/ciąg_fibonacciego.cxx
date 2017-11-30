79396b17d7666c3770926c02d4ce0d1f220d4798
mateusz0407 <jackowskim2000@wp.pl>
2017-11-30 19:43:57 +0100

    praca domowa

--- cpp/ciąg_fibonacciego.cxx
+++ cpp/ciąg_fibonacciego.cxx
@@ -9,11 +9,12 @@ using namespace std;
  
 int fib(int n)
 {
-  if(n<3)
-    return 1;
-    
-  if(n>2) 
-	return fib(n-2)+fib(n-1);
+	if(n<3)
+		return 1;
+		
+	if(n>2)
+		return fib(n-2)+fib(n-1);
+		
 }
 
  
