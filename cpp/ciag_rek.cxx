49c37ba652e2b6c658d81b32fb8b7ab3194ad45a
mateusz0407 <32060931+mateusz0407@users.noreply.github.com>
2017-12-15 07:50:11 +0100

    Update ciag_rek.cxx

--- cpp/ciag_rek.cxx
+++ cpp/ciag_rek.cxx
@@ -14,19 +14,19 @@ int wyraz(int a, int n)
 {
     if(n==1)
         return 2;
-    return (wyraz(a, n-1)-1) * ((wyraz(a, n-1)*2) +1 );
+    return ciag(n -1) * ciag (n * n) + 1
 }
  
 int main(int argc, char **argv)
 {
     int n=0;
-    int a=0;
-   
-    cout<<"Podaj 1 liczbe: ";
-    cin>>a;
+
+
+
+
     cout<<"Podaj 2 liczbe: ";
     cin>>n;
    
-    cout<<wyraz(a,n);
+    cout<<wyraz(n);
     return 0;
 }
