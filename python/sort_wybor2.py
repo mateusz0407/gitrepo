6da1dd0346e43aa87dc0f9d84a482e38004d3210
mateusz0407 <jackowskim2000@wp.pl>
2017-12-19 10:37:32 +0100

    babelki

---
+++ python/sort_wybor2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from random import randint
+
+def wypelnij(lista, ile, maks):
+    for i in range(ile):
+        lista.append(randint(0, maks))
+    return lista
+    
+def sort_wyb(lista):
+    for i in range(len(list)):
+        k = i
+        for j in range(i + 1, len(lista)):
+            if (lista[k]>lista[j]):
+                k = j
+        lista[i],lista[k]=lista[k],lista[i]
+        return lista
+        
+def main(args):
+    lista = []
+    print(wypelnij(lista, 10, 20))
+    print(sort_wyb(lista))
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
