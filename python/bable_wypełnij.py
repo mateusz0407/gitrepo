c43fb8e2147ff03631815494a220dfe6c0cea913
mateusz0407 <jackowskim2000@wp.pl>
2018-01-03 17:42:31 +0100

    praca domowa

--- python/bable_wypełnij.py
+++ python/bable_wypełnij.py
@@ -1,22 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def wstaw(lista):
-    for i in range(1,len(lista)):
+def sort_wstaw_max(lista):
+    """wersja liniowa"""
+    for i in range(1, len(lista)):
         el = lista[i]
-        j = i - 1
-        while j>=0 and lista[j]<el:# wyszukiwanie pozycji
-            lista[j + 1] = lista[j]# przesuwanie elementów
-            j = j - 1
-        lista[j + 1] = el
+        k = i - 1
+        while k >= 0 and lista[k] > el:  # wyszukuje pozycje do wstawienia
+            lista[k + 1] = lista[k]  # przesowanie w gore tabeli
+            k -= 1
+        lista[k + 1] = el  # wstawienie elementu
     return lista
 
-        
+
+def sort_wstaw_min(lista):
+    """wersja liniowa"""
+    for i in range(1, len(lista)):
+        el = lista[i]
+        k = i - 1
+        while k >= 0 and lista[k] < el:  # wyszukuje pozycje do wstawienia
+            lista[k + 1] = lista[k]  # przesowanie w gore tabeli
+            k -= 1
+        lista[k + 1] = el  # wstawienie elementu
+    return lista
+
+
 def main(args):
-    lista = [3, 4, 7, 0, 2, 3, 1, 9]
+    lista = [4, 3, 7, 0, 2, 3, 1, 9]
+    # [3, 4, 7, 0, 2, 3, 1, 9]
+    # [3, 4, 7, 0, 2, 3, 1, 9]
+    # [0, 3, 4, 7, 2, 3, 1, 9]
+
     print(lista)
-    print(wstaw(lista))
+    print("Posortowana lista od najwiekszej: ", sort_wstaw_max(lista))
+    print("Posortowana lista od najmniejszek: ", sort_wstaw_min(lista))
+
     return 0
+
 
 if __name__ == '__main__':
     import sys
