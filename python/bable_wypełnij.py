1cea3634d37b6599cd29d8fc345411d92c27bef3
mateusz0407 <jackowskim2000@wp.pl>
2018-01-09 10:37:41 +0100

    porzadkowanie

--- python/bable_wypełnij.py
+++ python/bable_wypełnij.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from math import floor
+
+
 def sort_wstaw_max(lista):
     """wersja liniowa"""
     for i in range(1, len(lista)):
@@ -23,13 +26,26 @@ def sort_wstaw_min(lista):
             k -= 1
         lista[k + 1] = el  # wstawienie elementu
     return lista
-    
-    
+
+
+def szukaj_bin(lewy, prawy, lista, el):
+    while lewy < prawy:
+        srodek = floor((lewy + prawy) / 2)
+        if el <= lista[srodek]:
+            prawy = srodek
+        else:
+            lewy = prawy + 1
+        return lewy
+
+
 def sort_wstaw_bin(lista):
     for i in range(1, len(lista)):
         el = lista[i]
         k = szukaj_bin(0, i, lista, el)
-        pass
+        lista = lista[: k] + [el] + lista[k: i] + lista[i + 1:]
+        print(lista)
+    return lista
+
 
 def main(args):
     lista = [4, 3, 7, 0, 2, 3, 1, 9]
@@ -40,7 +56,7 @@ def main(args):
     print(lista)
     print("Posortowana lista od najwiekszej: ", sort_wstaw_max(lista))
     print("Posortowana lista od najmniejszek: ", sort_wstaw_min(lista))
-
+    print(sort_wstaw_bin(lista))
     return 0
 
 
