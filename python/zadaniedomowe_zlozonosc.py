8daeebcf6eb0192a94b22eecdf7315c422fbf6e6
mateusz0407 <jackowskim2000@wp.pl>
2018-01-23 10:47:11 +0100

    poprawki

--- python/zadaniedomowe_zlozonosc.py
+++ python/zadaniedomowe_zlozonosc.py
@@ -5,14 +5,12 @@
 def main(args):
     n = int(input("Podaj liczbe: "))
     i = 2
-    while i * i >= n:
-        if n % i > 0:
-            print("pierwsze")
-            break
+    while i * i <= n:
+        if n % i == 0:
+            print("złożona")
+            return 0
         i += 1
-    while(n % i == 0):
-        print("Zlozona")
-        break
+    print("pierwsza")
     return 0
  
  
