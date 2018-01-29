77ea46250f528e3790fd650c7a4d3668514e8eef
mateusz0407 <jackowskim2000@wp.pl>
2018-01-29 12:36:44 +0100

    dec2bin

---
+++ python/konwersje.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def konwersja1(liczba10, podstawa):
+    """
+    funkcja zamienia liczbę dziesietna na system o podanekj podstawie
+    """
+    liczba = []  # lista reszt
+    while liczba10 != 0:
+        reszta = liczba10 % podstawa
+        if reszta > 9:
+            reszta = chr(reszta + 55)
+        liczba.append(str(reszta))
+        liczba10 = int(liczba10 / podstawa)
+
+    liczba.reverse()  # odwrócenie kolejności
+    return "".join(liczba)  # złączenie elemetów listy w ciąg
+
+
+def dec2other():
+    """
+    funkcja zamienia liczbę dziesietna na system o podanekj podstawie
+    """
+    liczba = int(input("podaj liczbę: "))
+    podstawa = int(input("podaj podstawe: "))
+    while podstawa < 2 or podstawa > 16:
+        podstawa = int(input("podaj podstawe: "))
+    print("wynik konwersji: {}(10)={}({})".format(
+        liczba, konwersja1(liczba, podstawa), podstawa))
+
+
+def konwersja2(liczba, podstawa)
+
+
+def other2dec():
+    print(konwersja2())
+
+
+def main(args):
+    print("zmiana liczby dziesiatną na liczbe o podajej podstawie"
+          "<2;16> lub odwrotnie")
+    dec2other()
+    other2dec()
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
